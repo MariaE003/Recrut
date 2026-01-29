0a20f1e525350c233882e80951474db6e40265fc
MariaE003 <elgotbymaria@gmail.com>
2026-01-29 23:34:34 +0100

    affichier les suggestions d'amis

--- app/Http/Controllers/AmisController.php
+++ app/Http/Controllers/AmisController.php
@@ -10,5 +10,13 @@ use Illuminate\Http\Request;
 
 class AmisController extends Controller
 {
+    public function show(){
+        $id=auth()->user()->id;
+        if ($id) {
+            // $amis=User::all();//li mkininch fi table amis
+        $amis=User::where('id',"!=",$id)->get();//li mkininch fi table amis + machi howa
+        return view('home',compact('amis'));        
+        } 
+    }
     
 }
